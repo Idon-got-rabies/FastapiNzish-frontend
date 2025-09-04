071eb650b2ad287f20108eb4d43e309b530d7c49
idon-got-rabies <davidmurage290@gmail.com>
2025-09-04 23:15:53 +0300

    adding loading animations to api calls v1.1.4.11

--- js/inventory.js
+++ js/inventory.js
@@ -24,6 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
       tbody.appendChild(row);
     });
     document.getElementById("nosales-section").style.display = "block";
+    hideLoader();
   })
   .catch(err => console.error(err));
   hideLoader();
